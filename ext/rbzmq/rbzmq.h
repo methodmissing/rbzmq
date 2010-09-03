ef4c488a457d799d4b63a42247d6613d8a3876e1
Lourens Naudé <lourens@methodmissing.com>
2010-09-03 02:07:37 +0100

    Vendor a 1.8 compatible rb_thread_blocking_region region implementation

--- ext/rbzmq/rbzmq.h
+++ ext/rbzmq/rbzmq.h
@@ -68,3 +68,40 @@ typedef unsigned __int64 uint64_t;
     void * s; \
     Data_Get_Struct (self_, void, s); \
     Check_Socket (s);
+
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+#define RB_ZMQ_UNUSED __attribute__ ((unused))
+#else
+#define RB_ZMQ_UNUSED
+#endif
+
+/* Credit to Brian Lopez and Eric Wong, leeched from mysql2 */
+
+/*
+ * partial emulation of the 1.9 rb_thread_blocking_region under 1.8,
+ * this is enough for dealing with blocking I/O functions in the
+ * presence of threads.
+ */
+
+#ifndef HAVE_RB_THREAD_BLOCKING_REGION
+
+#include <rubysig.h>
+#define RUBY_UBF_IO ((rb_unblock_function_t *)-1)
+typedef void rb_unblock_function_t(void *);
+typedef VALUE rb_blocking_function_t(void *);
+static VALUE
+rb_thread_blocking_region(
+  rb_blocking_function_t *func, void *data1,
+  RB_ZMQ_UNUSED rb_unblock_function_t *ubf,
+  RB_ZMQ_UNUSED void *data2)
+{
+  VALUE rv;
+
+  TRAP_BEG;
+  rv = func(data1);
+  TRAP_END;
+
+  return rv;
+}
+
+#endif /* ! HAVE_RB_THREAD_BLOCKING_REGION */
